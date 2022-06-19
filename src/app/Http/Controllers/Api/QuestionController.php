78750723c27fd0af3b41fcb57a693bb7016b2886
billiejoenahema <billiejoenahema@gmail.com>
2022-06-20 07:47:41 +0900

    feat: make post question backend api

---
+++ src/app/Http/Controllers/Api/QuestionController.php
@@ -0,0 +1,89 @@
+<?php
+
+namespace App\Http\Controllers\Api;
+
+use App\Http\Controllers\Controller;
+use App\Http\Requests\Question\StoreRequest;
+use App\Http\Resources\QuestionResource;
+use App\Models\Question;
+use Illuminate\Http\Request;
+use Illuminate\Support\Facades\DB;
+
+class QuestionController extends Controller
+{
+    /**
+     * Display a listing of the resource.
+     *
+     * @return \Illuminate\Http\Response
+     */
+    public function index()
+    {
+        //
+    }
+
+    /**
+     * 問題を追加する。
+     *
+     * @param StoreRequest $request
+     *
+     * @return \Illuminate\Http\Response
+     */
+    public function create(StoreRequest $request)
+    {
+        $question = DB::transaction(function () use ($request) {
+            $question = Question::create([
+                'word' => $request->word,
+                'correct_answer' => $request->correct_answer,
+                'example' => $request->example,
+            ]);
+            return $question;
+        });
+
+        return new QuestionResource($question);
+    }
+
+    /**
+     * Store a newly created resource in storage.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @return \Illuminate\Http\Response
+     */
+    public function store(Request $request)
+    {
+        //
+    }
+
+    /**
+     * Display the specified resource.
+     *
+     * @param  int  $id
+     * @return \Illuminate\Http\Response
+     */
+    public function show($id)
+    {
+        //
+    }
+
+    /**
+     * Update the specified resource in storage.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @param  int  $id
+     * @return \Illuminate\Http\Response
+     */
+    public function update(Request $request, $id)
+    {
+        //
+    }
+
+    /**
+     * Remove the specified resource from storage.
+     *
+     * @param  int  $id
+     * @return \Illuminate\Http\Response
+     */
+    public function destroy($id)
+    {
+        //
+    }
+}
---
+++ src/app/Http/Requests/Question/StoreRequest.php
@@ -0,0 +1,32 @@
+<?php
+
+namespace App\Http\Requests\Question;
+
+use Illuminate\Foundation\Http\FormRequest;
+
+class StoreRequest extends FormRequest
+{
+    /**
+     * Determine if the user is authorized to make this request.
+     *
+     * @return bool
+     */
+    public function authorize()
+    {
+        return true;
+    }
+
+    /**
+     * Get the validation rules that apply to the request.
+     *
+     * @return array<string, mixed>
+     */
+    public function rules()
+    {
+        return [
+            'word' => 'required|string|max:255',
+            'correct_answer' => 'required|string|max:255',
+            'example' => 'string|max:255',
+        ];
+    }
+}
