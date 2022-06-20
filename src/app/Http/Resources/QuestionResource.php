5063521f95209ba62180ee9dc7fb110f8fecd4fa
billiejoenahema <billiejoenahema@gmail.com>
2022-06-21 08:07:06 +0900

    feat: make question requests&resource

---
+++ src/app/Http/Resources/QuestionResource.php
@@ -0,0 +1,24 @@
+<?php
+
+namespace App\Http\Resources;
+
+use Illuminate\Http\Resources\Json\JsonResource;
+
+class QuestionResource extends JsonResource
+{
+    /**
+     * Transform the resource into an array.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @return array|\Illuminate\Contracts\Support\Arrayable|\JsonSerializable
+     */
+    public function toArray($request)
+    {
+        return [
+            'id' => $this->id,
+            'word' => $this->word,
+            'correct_answer' => $this->correct_answer,
+            'example' => $this->example,
+        ];
+    }
+}
