6d3753230337e9025946910a6ff0eb0df9f75676
billiejoenahema <billiejoenahema@gmail.com>
2022-06-22 08:43:23 +0900

    improve: move word create form to word list page

--- src/resources/js/pages/MainPage.vue
+++ src/resources/js/pages/MainPage.vue
@@ -1,35 +1,19 @@
 <script setup>
-import { reactive } from 'vue';
 import { useStore } from 'vuex';
 
 const store = useStore();
-
-const question = reactive({
-  word: '',
-  correct_answer: '',
-  example: '',
-});
-const addWord = () => {
-  console.log('clicked!');
-  store.dispatch('question/post', question);
-};
 </script>
 
 <template>
-  <h3>メインページ</h3>
+  <nav>
+    <router-link to="/">Home</router-link>
+    <router-link to="/word_list">List</router-link>
+  </nav>
+  <h3>英単語チェック</h3>
   <div>
     <label>単語</label>
     <p>issue</p>
     <label>正解</label>
     <p>answer</p>
   </div>
-  <form @submit.prevent>
-    <label>単語</label>
-    <input type="text" v-model="question.word" />
-    <label>正解</label>
-    <input type="text" v-model="question.correct_answer" />
-    <label>例文</label>
-    <input type="text" v-model="question.example" />
-    <button @click.prevent="addWord()">追加</button>
-  </form>
 </template>
--- src/resources/js/pages/WordList.vue
+++ src/resources/js/pages/WordList.vue
@@ -1,15 +1,41 @@
 <script setup>
-import { computed } from 'vue';
+import { computed, reactive } from 'vue';
 import { useStore } from 'vuex';
 
 const store = useStore();
 
 store.dispatch('question/get');
 const questions = computed(() => store.getters['question/data']);
+const newQuestion = reactive({
+  word: '',
+  correct_answer: '',
+  example: '',
+});
+const addWord = () => {
+  console.log('clicked!');
+  store.dispatch('question/post', newQuestion);
+};
 </script>
 
 <template>
+  <form @submit.prevent>
+    <h4>新規登録</h4>
+    <div class="column">
+      <label>単語</label>
+      <input type="text" v-model="newQuestion.word" />
+    </div>
+    <div class="column">
+      <label>正解</label>
+      <input type="text" v-model="newQuestion.correct_answer" />
+    </div>
+    <div class="column">
+      <label>例文</label>
+      <input type="text" v-model="newQuestion.example" />
+    </div>
+    <button @click.prevent="addWord()">追加</button>
+  </form>
   <div>
+    <h4>登録済み単語リスト</h4>
     <ul>
       <li v-for="question in questions" :key="question.id">
         {{ question.word }}
