e6e38cd5d905a83eb6f087f25e3dd1e388eb1aa9
billiejoenahema <billiejoenahema@gmail.com>
2022-07-23 08:47:36 +0900

    feat: update login page

--- src/resources/js/views/LoginPage.vue
+++ src/resources/js/views/LoginPage.vue
@@ -1,5 +1,40 @@
-<script setup></script>
+<script setup>
+import { computed, reactive } from 'vue';
+import { useRouter } from 'vue-router';
+import { useStore } from 'vuex';
+
+const router = useRouter();
+const store = useStore();
+
+const user = reactive({
+  email: '',
+  password: '',
+});
+const hasErrors = computed(() => store.getters['auth/hasErrors']);
+
+const login = async () => {
+  await store.dispatch('auth/login', user);
+  if (!hasErrors.value) {
+    router.push('/');
+  }
+};
+</script>
 
 <template>
-  <div>ログインページ</div>
+  <form class="column">
+    <p class="row">
+      <label for="login-email">Email</label>
+      <input v-model="user.email" id="login-email" name="email" type="email" />
+    </p>
+    <p class="row">
+      <label for="login-password">Password</label>
+      <input
+        v-model="user.password"
+        id="login-password"
+        name="password"
+        type="password"
+      />
+      <button @click.prevent.stop="login()">Sign in</button>
+    </p>
+  </form>
 </template>
