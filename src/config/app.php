a224687bc9378df06f6dd5b2a63cd1fcf8a50b55
billiejoenahema <billiejoenahema@gmail.com>
2022-08-18 08:24:59 +0900

    chore: install php-ide-helper

--- src/config/app.php
+++ src/config/app.php
@@ -186,6 +186,7 @@ return [
         /*
          * Package Service Providers...
          */
+        Barryvdh\LaravelIdeHelper\IdeHelperServiceProvider::class,
 
         /*
          * Application Service Providers...
