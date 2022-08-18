6bf47cc5be8ab8e1a6d98447378ca664aeedcec5
billiejoenahema <billiejoenahema@gmail.com>
2022-08-19 07:17:44 +0900

    chore: automatic generation of PHPDocs for models

--- src/app/Models/Question.php
+++ src/app/Models/Question.php
@@ -5,6 +5,29 @@ namespace App\Models;
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
 
+/**
+ * App\Models\Question
+ *
+ * @property int $id
+ * @property string $word 単語
+ * @property string $correct_answer 正解
+ * @property string|null $example 例文
+ * @property string|null $deleted_at
+ * @property \Illuminate\Support\Carbon|null $created_at
+ * @property \Illuminate\Support\Carbon|null $updated_at
+ * @method static \Database\Factories\QuestionFactory factory(...$parameters)
+ * @method static \Illuminate\Database\Eloquent\Builder|Question newModelQuery()
+ * @method static \Illuminate\Database\Eloquent\Builder|Question newQuery()
+ * @method static \Illuminate\Database\Eloquent\Builder|Question query()
+ * @method static \Illuminate\Database\Eloquent\Builder|Question whereCorrectAnswer($value)
+ * @method static \Illuminate\Database\Eloquent\Builder|Question whereCreatedAt($value)
+ * @method static \Illuminate\Database\Eloquent\Builder|Question whereDeletedAt($value)
+ * @method static \Illuminate\Database\Eloquent\Builder|Question whereExample($value)
+ * @method static \Illuminate\Database\Eloquent\Builder|Question whereId($value)
+ * @method static \Illuminate\Database\Eloquent\Builder|Question whereUpdatedAt($value)
+ * @method static \Illuminate\Database\Eloquent\Builder|Question whereWord($value)
+ * @mixin \Eloquent
+ */
 class Question extends Model
 {
     use HasFactory;
