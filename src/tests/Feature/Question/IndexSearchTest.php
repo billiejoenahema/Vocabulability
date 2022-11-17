976e363e95055f3230c63ce19f1cc9830bf5d18e
billiejoenahema <billiejoenahema@gmail.com>
2022-11-18 07:48:14 +0900

    test: make question index search test

---
+++ src/tests/Feature/Question/IndexSearchTest.php
@@ -0,0 +1,41 @@
+<?php
+
+namespace Tests\Feature\Question;
+
+use App\Models\Question;
+use App\Models\User;
+use Illuminate\Foundation\Testing\RefreshDatabase;
+use Tests\TestCase;
+
+class IndexSearchTest extends TestCase
+{
+    use RefreshDatabase;
+
+    /**
+     * テスト前の共通処理
+     */
+    public function setUp(): void
+    {
+        parent::setUp();
+
+        /** @var \Illuminate\Contracts\Auth\Authenticatable $user */
+        $this->user = User::factory()->create();
+        $question = Question::factory()->count(10)->create();
+
+        $this->word = $question[0]->word;
+        $this->correct_answer = $question[0]->correct_answer;
+    }
+
+    /**
+     * 単語で一覧を検索できるか確認するテスト
+     *
+     * @return void
+     */
+    public function test_searchIndexByWord()
+    {
+        $response = $this->actingAs($this->user)->getJson('/api/questions?keyword=' . $this->word);
+
+        $response->assertStatus(200)
+            ->assertJsonPath('data.0.word', $this->word);
+    }
+}
