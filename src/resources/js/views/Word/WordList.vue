9a821e1d8b61be5ac45c50319c275606838b072c
billiejoenahema <billiejoenahema@gmail.com>
2022-12-18 09:15:46 +0900

    feat: implement data cont in word list

--- src/resources/js/views/Word/WordList.vue
+++ src/resources/js/views/Word/WordList.vue
@@ -1,6 +1,7 @@
 <script setup>
 import { computed, onMounted, onUnmounted, ref } from 'vue';
 import { useStore } from 'vuex';
+import DataCount from '../../components/DataCount';
 import InvalidFeedback from '../../components/InvalidFeedback';
 import LoadingOverlay from '../../components/LoadingOverlay';
 import Pagination from '../../components/Pagination';
@@ -24,7 +25,7 @@ const invalidFeedback = computed(
 );
 const hasErrors = computed(() => store.getters['question/hasErrors']);
 const isInvalid = computed(() => store.getters['question/isInvalid']);
-const links = computed(() => store.getters['question/links']);
+const meta = computed(() => store.getters['question/meta']);
 const editable = ref([]);
 const isLoading = computed(() => store.getters['loading/isLoading']);
 const setIsLoading = (bool) => store.commit('loading/setIsLoading', bool);
@@ -122,6 +123,7 @@ onUnmounted(() => {
         </div>
       </div>
     </div>
+    <DataCount v-if="meta" :meta="meta" />
     <div class="row list-header">
       <div class="row" @click="onChangeSort('word')">
         <div class="list-column-title">単語</div>
@@ -204,5 +206,5 @@ onUnmounted(() => {
       </div>
     </div>
   </div>
-  <Pagination :links="links" @change="changePage" />
+  <Pagination :links="meta.links" @change="changePage" />
 </template>
