d45cd2cc342767d4fae0a0d64bda1203634271a7
billiejoenahema <billiejoenahema@gmail.com>
2023-03-04 09:13:10 +0900

    feat: make Tooltip component

---
+++ src/resources/js/components/Tooltip.vue
@@ -0,0 +1,42 @@
+<script setup>
+defineProps({
+  content: {
+    type: String,
+    required: false,
+    default: '',
+  },
+});
+</script>
+
+<template>
+  <div class="tooltip">
+    <slot />
+    <div class="description">{{ content }}</div>
+  </div>
+</template>
+
+<style>
+.tooltip {
+  position: relative;
+  cursor: pointer;
+  display: inline-block;
+}
+.tooltip p {
+  margin: 0;
+  padding: 0;
+}
+.description {
+  display: none;
+  position: absolute;
+  padding: 10px;
+  font-size: 12px;
+  line-height: 1.6em;
+  color: #fff;
+  border-radius: 5px;
+  background: #000;
+}
+
+.tooltip:hover .description {
+  display: flex;
+}
+</style>
