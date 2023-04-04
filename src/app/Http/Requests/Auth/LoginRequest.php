d4f40bbb1099201c2b0eba3f4c257bfbd5a344b9
billiejoenahema <billiejoenahema@gmail.com>
2023-04-05 07:54:21 +0900

    improve: email validation

--- src/app/Http/Requests/Auth/LoginRequest.php
+++ src/app/Http/Requests/Auth/LoginRequest.php
@@ -24,7 +24,7 @@ final class LoginRequest extends FormRequest
     public function rules(): array
     {
         return [
-            'email' => 'required|string|email:rfc,spoof',
+            'email' => 'required',
             'password' => 'required',
         ];
     }
